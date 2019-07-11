e1a84e34103bd3d4b8c533a3ed9b79017336a00b
Petre Rodan <c59ccef70aa1387612c4@subdimension.ro>
2019-07-11 16:02:06 +0300

    cleanup

--- tests/gt9xx/gt9xx.c
+++ tests/gt9xx/gt9xx.c
@@ -40,27 +40,27 @@
 //#define READ_CONFIG_DURING_INIT
 
 // factory config
-    uint8_t gt9xx_conf[GT9XX_CONFIG_911_SZ] =
-        { 0x41, 0x20, 0x03, 0xe0, 0x01, 0x05, 0x3d, 0x0, 0x01, 0x08,
-          0x1e, 0x05, 0x50, 0x3c, 0x03, 0x05, 0x0, 0x0, 0x0, 0x0,
-          0x0, 0x0, 0x0, 0x1a, 0x1c, 0x1e, 0x14, 0x8c, 0x2e, 0x0e,
-          0x28, 0x2a, 0x5e, 0x05, 0x0, 0x0, 0x01, 0x9b, 0x03, 0x11,
-          0x0, 0x0, 0x0, 0x0, 0x0, 0x03, 0x64, 0x32, 0x0, 0x0,
-          0x0, 0x19, 0x41, 0x94, 0xc5, 0x02, 0x07, 0x0, 0x0, 0x04,
-          0xb9, 0x1b, 0x0, 0x99, 0x21, 0x0, 0x7f, 0x28, 0x0, 0x69,
-          0x31, 0x0, 0x59, 0x3b, 0x0, 0x59, 0x0, 0x0, 0x0, 0x0,
-          0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-          0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-          0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-          0x0, 0x0, 0x1c, 0x1a, 0x18, 0x16, 0x14, 0x12, 0x10, 0x0e,
-          0x0c, 0x0a, 0x08, 0x06, 0x04, 0x02, 0x0, 0x0, 0x0, 0x0,
-          0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-          0x0, 0x0, 0x2a, 0x29, 0x28, 0x26, 0x24, 0x22, 0x21, 0x20,
-          0x1f, 0x1e, 0x1d, 0x1c, 0x18, 0x16, 0x14, 0x13, 0x12, 0x10,
-          0x0f, 0x0c, 0x0a, 0x08, 0x06, 0x04, 0x02, 0x0, 0x0, 0x0,
-          0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-          0x0, 0x0, 0x0, 0x0, 0xef, 0x0,
-    };
+uint8_t gt9xx_conf[GT9XX_CONFIG_911_SZ] =
+    { 0x41, 0x20, 0x03, 0xe0, 0x01, 0x05, 0x3d, 0x0, 0x01, 0x08,
+    0x1e, 0x05, 0x50, 0x3c, 0x03, 0x05, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x1a, 0x1c, 0x1e, 0x14, 0x8c, 0x2e, 0x0e,
+    0x28, 0x2a, 0x5e, 0x05, 0x0, 0x0, 0x01, 0x9b, 0x03, 0x11,
+    0x0, 0x0, 0x0, 0x0, 0x0, 0x03, 0x64, 0x32, 0x0, 0x0,
+    0x0, 0x19, 0x41, 0x94, 0xc5, 0x02, 0x07, 0x0, 0x0, 0x04,
+    0xb9, 0x1b, 0x0, 0x99, 0x21, 0x0, 0x7f, 0x28, 0x0, 0x69,
+    0x31, 0x0, 0x59, 0x3b, 0x0, 0x59, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x1c, 0x1a, 0x18, 0x16, 0x14, 0x12, 0x10, 0x0e,
+    0x0c, 0x0a, 0x08, 0x06, 0x04, 0x02, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x2a, 0x29, 0x28, 0x26, 0x24, 0x22, 0x21, 0x20,
+    0x1f, 0x1e, 0x1d, 0x1c, 0x18, 0x16, 0x14, 0x13, 0x12, 0x10,
+    0x0f, 0x0c, 0x0a, 0x08, 0x06, 0x04, 0x02, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0xef, 0x0,
+};
 
 uint8_t gt9xx_coord_buff[GT9XX_COORD_MAX_COUNT * GT9XX_POINT_STRUCT_SZ];
 
@@ -77,7 +77,6 @@ static void gt9xx_event_handler(uint16_t msg)
     GT9XX_enable_irq();
 }
 
-
 void gt9xx_rst_event(void)
 {
     gt9xx_last_event = GT9XX_EV_NULL;
@@ -88,7 +87,7 @@ uint8_t gt9xx_get_event(void)
     return gt9xx_last_event;
 }
 
-uint8_t GT9XX_read(struct goodix_ts_data *t, const uint16_t reg,
+uint8_t GT9XX_read(struct goodix_ts_data * t, const uint16_t reg,
                    uint8_t * buf, const size_t buf_len)
 {
     uint8_t rv = 0;
@@ -110,7 +109,7 @@ uint8_t GT9XX_read(struct goodix_ts_data *t, const uint16_t reg,
     return rv;
 }
 
-uint8_t GT9XX_write(struct goodix_ts_data *t, const uint16_t reg,
+uint8_t GT9XX_write(struct goodix_ts_data * t, const uint16_t reg,
                     uint8_t * buf, const size_t buf_len)
 {
     uint8_t rv = 0;
@@ -132,41 +131,41 @@ uint8_t GT9XX_write(struct goodix_ts_data *t, const uint16_t reg,
     return rv;
 }
 
-int16_t GT9xx_check_cfg_8(struct goodix_ts_data *t,
-			const struct firmware *cfg)
+int16_t GT9xx_check_cfg_8(struct goodix_ts_data * t, const struct firmware * cfg)
 {
-	int i, raw_cfg_len = cfg->size - 2;
-	uint8_t check_sum = 0;
+    int i, raw_cfg_len = cfg->size - 2;
+    uint8_t check_sum = 0;
 
-	for (i = 0; i < raw_cfg_len; i++)
-		check_sum += cfg->data[i];
-	check_sum = (~check_sum) + 1;
+    for (i = 0; i < raw_cfg_len; i++)
+        check_sum += cfg->data[i];
+    check_sum = (~check_sum) + 1;
 
-	if (check_sum != cfg->data[raw_cfg_len]) {
+    if (check_sum != cfg->data[raw_cfg_len]) {
         // wrong checksum
-		return GT9XX_err_check_cfg_checksum;
-	}
+        return GT9XX_err_check_cfg_checksum;
+    }
 
-	if (cfg->data[raw_cfg_len + 1] != 1) {
-	    // config must have Config_Fresh register set
-		return GT9XX_err_check_cfg_fresh;
-	}
+    if (cfg->data[raw_cfg_len + 1] != 1) {
+        // config must have Config_Fresh register set
+        return GT9XX_err_check_cfg_fresh;
+    }
 
-	return 0;
+    return 0;
 }
 
-uint8_t GT9XX_calc_checksum(uint8_t* buf, uint16_t len) {
-  uint8_t ret = 0;
-  uint8_t i;
+uint8_t GT9XX_calc_checksum(uint8_t * buf, uint16_t len)
+{
+    uint8_t ret = 0;
+    uint8_t i;
 
-  for (i = len - 1; i > 0; i--) {
-    ret += buf[i-1];
-  }
-  ret = (~ret) + 1;
-  return ret;
+    for (i = len - 1; i > 0; i--) {
+        ret += buf[i - 1];
+    }
+    ret = (~ret) + 1;
+    return ret;
 }
 
-uint8_t GT9XX_init(struct goodix_ts_data *t)
+uint8_t GT9XX_init(struct goodix_ts_data * t)
 {
     uint8_t rv = 0;
     char itoa_buf[18];
@@ -176,7 +175,7 @@ uint8_t GT9XX_init(struct goodix_ts_data *t)
     timer_a1_delay_ccr2(_100ms);
 
     // reset
-    
+
     // set both control pins as output, low level
     set_irq_output;
     set_irq_low;
@@ -191,27 +190,26 @@ uint8_t GT9XX_init(struct goodix_ts_data *t)
     if (t->slave_addr == 0x14) {
         set_irq_high;
     }
-   
     // T3 > 100us
-    timer_a1_delay_ccr2(2*_100us);
+    timer_a1_delay_ccr2(2 * _100us);
     set_rst_high;
     //set_rst_input;
 
     // T4 > 5ms
-    timer_a1_delay_ccr2(6*_1ms);
+    timer_a1_delay_ccr2(6 * _1ms);
     // set_irq_low; // this will actually set a pulldown resistor
     // slave address selection has ended
 
     // T5 > 50ms
-    timer_a1_delay_ccr2(51*_1ms);
-    set_irq_input;  
+    timer_a1_delay_ccr2(51 * _1ms);
+    set_irq_input;
 
     //timer_a1_delay_ccr2(51*_1ms);
     timer_a1_delay_ccr2(_200ms);
 
     // send configuration
 
-    ts_handled = t; // kinda cludgy
+    ts_handled = t;             // kinda cludgy
     sys_messagebus_register(&gt9xx_event_handler, SYS_MSG_GT9XX_IRQ);
 
     rv = GT9XX_read_version(t);
@@ -227,14 +225,12 @@ uint8_t GT9XX_init(struct goodix_ts_data *t)
     if (rv) {
         return GT9XX_err_check_chip_id;
     }
-
 #ifdef READ_CONFIG_DURING_INIT
     // optionally read the config registers (this will malloc() 186 bytes)
     rv = GT9XX_read_config(t);
     if (rv) {
         return rv;
     }
-
     // fake a fresh config so it's test passes the _check_config()
     t->conf.data[rOFF_FRESH_CONFIG] = 1;
 
@@ -247,7 +243,7 @@ uint8_t GT9XX_init(struct goodix_ts_data *t)
     return rv;
 }
 
-uint8_t GT9XX_clear_irq(struct goodix_ts_data *t)
+uint8_t GT9XX_clear_irq(struct goodix_ts_data * t)
 {
     uint8_t rv = 0;
     uint8_t data[1] = { 0 };
@@ -260,18 +256,18 @@ uint8_t GT9XX_clear_irq(struct goodix_ts_data *t)
 uint8_t GT9XX_check_chipid(const uint16_t id)
 {
     switch (id) {
-	case 911:
-	case 9110:
-	case 927:
-	case 9271:
-	case 928:
+    case 911:
+    case 9110:
+    case 927:
+    case 9271:
+    case 928:
         return EXIT_SUCCESS;
     }
 
     return EXIT_FAILURE;
 }
 
-uint8_t GT9XX_read_version(struct goodix_ts_data *t)
+uint8_t GT9XX_read_version(struct goodix_ts_data * t)
 {
     uint8_t rv = EXIT_FAILURE;
     char buf[11];
@@ -287,12 +283,12 @@ uint8_t GT9XX_read_version(struct goodix_ts_data *t)
     return rv;
 }
 
-uint8_t GT9XX_read_config(struct goodix_ts_data *t)
+uint8_t GT9XX_read_config(struct goodix_ts_data * t)
 {
     uint8_t *config_data;
     int16_t rv = EXIT_SUCCESS;
-    
-    config_data = (uint8_t *) malloc(sizeof(uint8_t)*GT9XX_CONFIG_911_SZ);
+
+    config_data = (uint8_t *) malloc(sizeof(uint8_t) * GT9XX_CONFIG_911_SZ);
     if (config_data == NULL) {
         return GT9XX_err_malloc;
     }
@@ -308,7 +304,7 @@ uint8_t GT9XX_read_config(struct goodix_ts_data *t)
     return rv;
 }
 
-uint8_t GT9XX_write_config(struct goodix_ts_data *t)
+uint8_t GT9XX_write_config(struct goodix_ts_data * t)
 {
     int16_t rv = EXIT_SUCCESS;
 
@@ -321,7 +317,6 @@ uint8_t GT9XX_write_config(struct goodix_ts_data *t)
     if (rv) {
         return GT9XX_err_i2c_write;
     }
-
     // allow the TS chip to reconfigure itself
     timer_a1_delay_ccr2(_200ms);
 
@@ -345,7 +340,7 @@ int16_t GT9XX_read_state(struct goodix_ts_data *t, uint8_t * data)
     uint8_t i;
     struct GTPoint *coord;
 
-    coord = (struct GTPoint *) gt9xx_coord_buff;
+    coord = (struct GTPoint *)gt9xx_coord_buff;
 
     rv = GT9XX_read(t, GT9XX_rCOORD_ADDR, reply, 1);
 
@@ -361,14 +356,15 @@ int16_t GT9XX_read_state(struct goodix_ts_data *t, uint8_t * data)
 
     coord_cnt = reply[0] & 0x0f;
 
-    if (coord_cnt > 0 && coord_cnt <= GT9XX_COORD_MAX_COUNT ) {
+    if (coord_cnt > 0 && coord_cnt <= GT9XX_COORD_MAX_COUNT) {
         uart0_print("c");
         uart0_print(_utoa(itoa_buf, coord_cnt));
         uart0_print("\r\n");
 
-        rv = GT9XX_read(t, GT9XX_rCOORD_ADDR + 1, gt9xx_coord_buff, GT9XX_POINT_STRUCT_SZ * (coord_cnt));
+        rv = GT9XX_read(t, GT9XX_rCOORD_ADDR + 1, gt9xx_coord_buff,
+                        GT9XX_POINT_STRUCT_SZ * (coord_cnt));
 
-        for (i=0; i<coord_cnt; i++) {
+        for (i = 0; i < coord_cnt; i++) {
             uart0_print(" ");
             uart0_print(_utoa(itoa_buf, coord[i].trackId));
             uart0_print(" ");
@@ -379,7 +375,7 @@ int16_t GT9XX_read_state(struct goodix_ts_data *t, uint8_t * data)
             uart0_print(_utoa(itoa_buf, coord[i].area));
             uart0_print("\r\n");
         }
-        
+
         //if (rv) {
         //    return -rv;
         //}
@@ -395,26 +391,27 @@ int16_t GT9XX_read_state(struct goodix_ts_data *t, uint8_t * data)
 
 void GT9XX_disable_irq(void)
 {
-    P6IE &= ~GT9XX_IRQ;  // disable interrupt
-    P6IFG &= ~GT9XX_IRQ; // clear interrupt
+    P6IE &= ~GT9XX_IRQ;         // disable interrupt
+    P6IFG &= ~GT9XX_IRQ;        // clear interrupt
 }
 
 void GT9XX_enable_irq(void)
 {
-    P6IFG &= ~GT9XX_IRQ; // clear interrupt
-    P6IE |= GT9XX_IRQ;  // enable interrupt
+    P6IFG &= ~GT9XX_IRQ;        // clear interrupt
+    P6IE |= GT9XX_IRQ;          // enable interrupt
 }
 
-uint16_t _strtou16(char *buf) {
+uint16_t _strtou16(char *buf)
+{
     uint16_t val = 0;
     uint8_t c;
     uint8_t i;
 
-    if ( (buf[0] < 0x30) || (buf[0] > 0x46) || ( buf[0] == 0x40 ) ) {
+    if ((buf[0] < 0x30) || (buf[0] > 0x46) || (buf[0] == 0x40)) {
         return 0;
     }
 
-    for (i=0; i<4; i++) {
+    for (i = 0; i < 4; i++) {
         c = buf[i];
         if (c == 0) {
             return val;
@@ -424,11 +421,10 @@ uint16_t _strtou16(char *buf) {
         } else {
             c -= 55;
         }
-        val = val*10 + c;
+        val = val * 10 + c;
     }
     return val;
 }
-
 
 // Port 6 interrupt service routine
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
@@ -447,6 +443,5 @@ void Port6_ISR(void)
         LPM3_EXIT;
     }
 }
-
 
 #endif                          // __I2C_CONFIG_H__
